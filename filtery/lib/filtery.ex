9ede91ff70cdb168ba27ce9f4f0e8a5f2a2e65f6
Dung Nguyen <dung.nguyen@onpoint.vn>
2021-07-20 09:36:10 +0700

    initialize repository

---
+++ filtery/lib/filtery.ex
@@ -0,0 +1,18 @@
+defmodule Filtery do
+  @moduledoc """
+  Documentation for `Filtery`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Filtery.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ filtery/test/filtery_test.exs
@@ -0,0 +1,8 @@
+defmodule FilteryTest do
+  use ExUnit.Case
+  doctest Filtery
+
+  test "greets the world" do
+    assert Filtery.hello() == :world
+  end
+end
