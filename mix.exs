6338f53c81608fd44f1eeac0a2ceb108043e56c9
Dung Nguyen <dung.nguyen@onpoint.vn>
2021-07-21 11:49:33 +0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Filtery.MixProject do
   def project do
     [
       app: :filtery,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -40,7 +40,7 @@ defmodule Filtery.MixProject do
 
   defp description() do
     """
-    Compose your dynamic query with Mongo like filter
+    Compose your dynamic query with Mongo like query syntax
     """
   end
 
