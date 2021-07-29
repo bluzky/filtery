a084fdd2c91b917df3cb07aa18700eab106c547a
Dung Nguyen <dung.nguyen@onpoint.vn>
2021-07-29 11:50:56 +0700

    Fix version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Filtery.MixProject do
     [
       app: :filtery,
       version: "0.2.3",
-      elixir: "~> 1.13",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: docs(),
