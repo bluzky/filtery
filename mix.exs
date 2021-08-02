367036434b90b54e34618df3c91da303231a0a0d
Dung Nguyen <dung.nguyen@onpoint.vn>
2021-08-02 21:57:52 +0700

    add test coveralls

--- mix.exs
+++ mix.exs
@@ -8,6 +8,10 @@ defmodule Filtery.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test
+      ],
       docs: docs(),
       name: "Filtery",
       description: description(),
@@ -50,7 +54,8 @@ defmodule Filtery.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:ecto, "~> 3.0"},
-      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.14", only: :test}
     ]
   end
 end
